813d562b999a548ab7817187ca095402bf20d319
mng9634 <mng9634@cs.rit.edu>
2016-09-17 01:16:03 -0400

    test

---
+++ src/BigRed.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Miles on 9/16/2016.
+ */
+public class BigRed {
+    public static void main(String[] args) {
+        choice
+        player one = new player(choice);
+        int currentLevel =
+    }
+    public player battleLoop(){}
+
+    public class player{
+        int level;
+        String playerClass;
+        String name;
+        public player(String choice){
+            this.level = 1;
+
+        }
+        public int getLevel(player one){
+            return one.level;
+        }
+    }
+    public class enemy{
+        public enemy(String name){
+
+        }
+    }
+}
