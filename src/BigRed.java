a5c9a44704abdd6fbeb591aa48b5528ec5dc8af8
mng9634 <mng9634@cs.rit.edu>
2016-09-17 15:43:48 -0400

    Equip and unequip items now works

--- src/BigRed.java
+++ src/BigRed.java
@@ -4,31 +4,45 @@
 import java.util.Scanner;
 public class BigRed {
     public static void main(String[] args) {
-        String choice;
+        String choice = " ";
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome. Choose the class you would like to be.");
-        System.out.println("Knight. Mage. Rouge");
-        choice = input.nextLine();
+        while(true) {
+            System.out.println("Welcome. Choose the class you would like to be.");
+            System.out.println("Knight. Mage. Rouge");
+            choice = input.nextLine();
+            if(choice.equals("Knight")){
+                break;
+            } else if(choice.equals("Mage")){
+                break;
+            } else if(choice.equals("Rouge")) {
+                break;
+            } else{
+                System.out.println("That is not a valid Class.");
+            }
+        }
         System.out.println("What would you like your character's name to be?");
         String name = input.nextLine();
         player one = new player(choice,name);
         standardLoop(one);
+        System.out.println("all done.");
     }
     public static void standardLoop(player one){
-        String choice2 = " ";
-        while(choice2!= "quit") {
+        String choice = " ";
+        while(!choice.equals("quit")) {
             Scanner input = new Scanner(System.in);
             System.out.println("What would you like to do?");
-            choice2 = input.nextLine();
-            if (choice2 == "equip"){
+            choice = input.nextLine();
+            if (choice.equals("equip")){
                 System.out.println("What would you like to equip?");
-                choice2 = input.nextLine();
-                one.equip(choice2);
-            } else if(choice2 == "unequip"){
+                choice = input.nextLine();
+                one.equip(choice);
+            } else if(choice.equals("unequip")){
                 System.out.println("What would you like to unequip.");
-                choice2 = input.nextLine();
-                one.unequip(choice2);
-            } else{
+                choice = input.nextLine();
+                one.unequip(choice);
+            } else if (choice.equals("stats")){
+                one.printStats();
+            }else {
                 System.out.println("That is not a valid choice.");
             }
         }
