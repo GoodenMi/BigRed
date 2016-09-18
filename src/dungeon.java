996a6701105bbabd51fee83e62ab476caefd2533
elmoundo <elmoundo222@gmail.com>
2016-09-17 23:02:51 -0400

    Add files via upload

--- src/dungeon.java
+++ src/dungeon.java
@@ -2,15 +2,53 @@
 public class dungeon {
 	String name;
 	String location;
-	int numLvl;
+	int size;
 	int numMonsters;
-	public dungeon(String name, String location, int numLvl, int numMonsters){
+	int currentLevel;
+	int loot;
+	item itemLoot;
+	public dungeon(String name, String location, int size, int numMonsters, int currentLevel, int loot, item itemLoot){
 		this.name=name;
 		this.location=location;
-		this.numLvl=numLvl;
+		this.size=size;
 		this.numMonsters=numMonsters;
+		this.currentLevel=0;
+		this.loot=loot;
+		this.itemLoot=item.getName();
 	}
 	
+	public void killedMonster(){
+		if (){
+			numMonsters--;
+		}
+	}
+		
+	public void increaseLevel(){
+		if (numMonsters==0 && currentLevel!=size){
+			currentLevel++;
+		}
+	}
+	
+	public void exit(){
+		//TODO exit the dungeon
+	}
+	
+	public boolean completed();
+		if (numMonsters==0 && curentLevel==size){
+			//TODO add item to invitory
+			player.gold=player.gold+loot;
+			return true;
+		}
+	
+	/* getter functions*/
+	
+	public getSize(){
+		return this.size;
+	}
+	
+	public getLevel(){
+		return this.currentLevel;
+	}
 	
 	
 	public String getName() {
